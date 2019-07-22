2808248daed732a1b5c0ded8edec25dcfe505ed6
aadebuger <zhvxxh@gmail.com>
2019-07-22 13:29:29 +0800

    test

--- visualize_cv2.py
+++ visualize_cv2.py
@@ -54,6 +54,47 @@ colors = random_colors(len(class_names))
 class_dict = {
     name: color for name, color in zip(class_names, colors)
 }
+serialid=0
+def totempimage(image1, mask, color, alpha=0.5):
+    """apply mask to image"""
+    global serialid
+#    image = np.zeros(image1.shape[0:2], dtype = "uint8")
+    image = image1.copy()
+ #   cv2.rectangle(rectangle, (25, 25), (275, 275), 255, -1)
+    for n, c in enumerate(color):
+        image[:, :, n] = np.where(
+            mask == 1,
+#            image[:, :, n] * (1 - alpha) + alpha * c,
+            0,
+#            image[:, :, n]
+            255
+        )
+ 
+    cv2.imwrite("temp{0}.jpg".format(serialid),image)
+    torect("temp{0}.jpg".format(serialid),serialid)
+    serialid=serialid+1
+def torect(filename,serialid):
+
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+    print("serialid=",serialid)
+    contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    print("contours=",len(contours))
+    cv2.drawContours(img,contours[1:],-1,(0,0,255),3)
+
+    for i in  range(0, len(contours)):
+            rect=cv2.minAreaRect(contours[i])
+            print("i=",i," rect=",rect)
+            box = np.int0(cv2.boxPoints(rect))
+            print(box)
+#            x, y, w, h = cv2.boundingRect(contours[0]) 
+#            print(x,y)
+            cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+
+
+    cv2.imwrite("recttemp{0}.jpg".format(serialid),img)
+
 
 
 def apply_mask(image, mask, color, alpha=0.5):
@@ -88,7 +129,10 @@ def display_instances(image, boxes, masks, ids, names, scores):
         score = scores[i] if scores is not None else None
         caption = '{} {:.2f}'.format(label, score) if score else label
         mask = masks[:, :, i]
-
+        print("x1=",x1,"y1=",y1,"x2=",x2,"y2=",y2)
+#        print("score=",score)
+        print("label=",label)
+        totempimage(image,mask,color)
         image = apply_mask(image, mask, color)
         image = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
         image = cv2.putText(
@@ -96,6 +140,41 @@ def display_instances(image, boxes, masks, ids, names, scores):
         )
 
     return image
+
+def display_instance(image, boxes, masks, ids, names, scores,instanceid):
+    """
+        take the image and results and apply the mask, box, and Label
+    """
+    n_instances = boxes.shape[0]
+
+    if not n_instances:
+        print('NO INSTANCES TO DISPLAY')
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+
+    for i in range(n_instances):
+        if not np.any(boxes[i]):
+            continue
+        if i != instanceid:
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        label = names[ids[i]]
+        color = class_dict[label]
+        score = scores[i] if scores is not None else None
+        caption = '{} {:.2f}'.format(label, score) if score else label
+        mask = masks[:, :, i]
+        print("x1=",x1,"y1=",y1,"x2=",x2,"y2=",y2)
+#        print("score=",score)
+        print("label=",label)
+        totempimage(image,mask,color)
+        image = apply_mask(image, mask, color)
+        image = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        image = cv2.putText(
+            image, caption, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+        )
+
+    return image
+
 
 
 if __name__ == '__main__':
